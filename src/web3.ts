90c903ff351f89f944a883fe01195eeae8c5762c
Sky-me-Rainbow <rain.bow.yellow.ace@gmail.com>
2025-08-30 04:17:16 +0900

    fix: fix public ETH_RPC_URL
    Co-authored-by: prince777-k <b.laurent.ace@gmail.com>
    Co-authored-by: Sky-me-Rainbow <rain.bow.yellow.ace@gmail.com>

--- src/web3.ts
+++ src/web3.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 
-const USDT_MAINNET_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const USDT_MINT_ADDRESS = process.env.USDT_MINT_ADDRESS || '0xdAC17F958D2ee523a2206206994597C13D831ec7';
 const ERC20_ABI = [
 	// balanceOf(address)
 	'function balanceOf(address owner) view returns (uint256)',
@@ -9,10 +9,10 @@ const ERC20_ABI = [
 ];
 
 function getProvider(): ethers.JsonRpcProvider {
-	const rpcUrl = process.env.ETH_RPC_URL;
-	if (!rpcUrl) {
-		throw new Error('Missing RPC URL. Set ETH_RPC_URL in .env');
-	}
+	const rpcUrl = process.env.ETH_RPC_URL || 'https://ethereum-rpc.publicnode.com';
+	// if (!rpcUrl) {
+	// 	throw new Error('Missing RPC URL. Set ETH_RPC_URL in .env');
+	// }
 	return new ethers.JsonRpcProvider(rpcUrl);
 }
 
@@ -28,7 +28,7 @@ export async function getEthBalance(address: string): Promise<string> {
 	return ethers.formatEther(balanceWei);
 }
 
-export async function getUsdtBalance(address: string, tokenAddress: string = USDT_MAINNET_ADDRESS): Promise<string> {
+export async function getUsdtBalance(address: string, tokenAddress: string = USDT_MINT_ADDRESS): Promise<string> {
 	const provider = getProvider();
 	const erc20 = new ethers.Contract(tokenAddress, ERC20_ABI, provider);
 	const [raw, decimals] = await Promise.all([
